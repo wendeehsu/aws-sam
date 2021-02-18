6fa5b7789ff99580a45410a6bc001e2b0c392364
Melhor <b05705046@ntu.edu.tw>
2021-02-18 22:54:57 +0800

    add get list api

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,15 +1,35 @@
 import json
+import boto3
+from flask import request, jsonify
 from flask_lambda import FlaskLambda
 
 app = FlaskLambda(__name__)
+ddb = boto3.resource("dynamodb")
+table = ddb.Table("students")
 
 @app.route('/hello')
 def index():
-    data = {
-        'message': 'Hello from aws'
-    }
+    data = {'message': 'Hello from aws'}
     return (
-        json.dumps(data),
+        jsonify(data),
         200,
         {'Content-Type': 'application/json'}
     )
+
+@app.route('/students', methods=['GET', 'POST'])
+def put_or_list_students():
+    if request.method == "GET":
+        students = table.scan()["Items"]
+        return (
+            jsonify(students),
+            200,
+            {'Content-Type': 'application/json'}
+        )
+
+    else:  #request.method == "POST"
+        table.put_item(Item=request.form.to_dict())
+        return (
+            json.dumps({"message":"item inserted!"}),
+            200,
+            {'Content-Type': 'application/json'}
+        )
